17399e254cdc982f14c0ee66826653172238cdeb
arielanides <arielsmania@gmail.com>
2026-03-13 18:25:53 -0300

    fix supabase config

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://bcvwqwofmabtvliphjtw.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjdndxd29mbWFidHZsaXBoanR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYxMjM0MDIsImV4cCI6MjA2MTY5OTQwMn0.oj3OFCDGlbfKqMSGKkmtMOTGWVW_5XavNoaJfvzz4OA'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
