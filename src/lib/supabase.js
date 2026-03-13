6d80aae3fac775d305e2abb080f0bd4ee9cadd26
arielanides <arielsmania@gmail.com>
2026-03-13 19:16:47 -0300

    new supabase project

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://bcvwqwofmabtvliphjtw.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjdndxd29mbWFidHZsaXBoanR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYxMjM0MDIsImV4cCI6MjA2MTY5OTQwMn0.oj3OFCDGlbfKqMSGKkmtMOTGWVW_5XavNoaJfvzz4OA'
+const supabaseUrl = 'https://bytrcuolcqlirpdvsqzd.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5dHJjdW9sY3FsaXJwZHZzcXpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0Mzc3MjksImV4cCI6MjA4OTAxMzcyOX0.jdIJ_ppbH6HjkpQp1J256U5oCnusSb2P9zbGHqQ70UM'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
